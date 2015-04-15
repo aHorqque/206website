10e601bbc8391ed75ce3dad244e30cb21d661041
Sophia LIM <slim44@teaching.CS.McGill.CA>
2015-04-15 19:59:55 -0400

    Added isMember function

--- cgi-bin/MyFacebookPage.py
+++ cgi-bin/MyFacebookPage.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python
+#/usr/bin/env python
 import cgi
 import csv
+import os
 
 postForm = cgi.FieldStorage()
 currentusername = postForm.getvalue("username")
@@ -15,16 +16,28 @@ def printMembers():
 		print "</ul>"
 
 #searches if element of row is friend of current user
+#returns false if member is not a friend; returns true if member is a friend
 def isFriend(elementOfTopic):
-	foundFriend = False
+	foundFriend = False #default: is not friend
 	with open('../data/members.csv','r') as csvfile:
 		reader = csv.reader(csvfile)
 		for row in reader:
 			if row[1] == currentusername: #finds row of current user
 				for i, element in enumerate(row, start=3):
 					if element == elementOfTopic:
-						foundFriend = True
+						foundFriend = True #is friend
 	return foundFriend
+
+#search if element of row is member
+#eturn false if it is a member; returns true if it is a member
+def isMember(member):
+	foundMember = False
+	with open('../data/members.csv','r') as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			if row[1] == member:
+				foundMember = True
+	return foundMember
 
 #prints user's friends list (up to 10 posts)
 def printFeed():
@@ -32,26 +45,28 @@ def printFeed():
 	print_feed = []
 	counter = 1 #keep track of the line we are at (odd = user, even = topic)
 	topicFromFriend =0 #We know if the next line will be a topic from friend
-	with open('../data/topic.csv', 'r') as csvfile:
-		reader = csv.reader(csvfile)
-		for row in reader:	
-			if counter%2==0:
-				if topicFromFriend==1:
-					new_rows_list.append((str(row).strip('[]')).strip("''"))		
-			elif isFriend(row[0]) == True:
-				new_rows_list.append("<b>" + (str(row).strip('[]')).strip("''") + "</b>")
-				topicFromFriend = 1
-			else: #the person is not a friend, so next line not to be appended
-				topicFromFriend =0 			
-			counter=counter+1	
-	counter = 0
-	for feed in reversed(new_rows_list):
-		if counter < 20: #counting until 20 because it should be the 20 last fields
- 			print_feed.append(feed)
-			counter = counter+1
-	print_feed.reverse() #because right now it is topic - user, rather than user, topic
-	print "<br>".join(map(str, print_feed))	
-
+	if (os.stat('../data/topic.csv').st_size != 0): #if file is not empty
+		with open('../data/topic.csv', 'r') as csvfile:
+			reader = csv.reader(csvfile)
+			for row in reader:	
+				if counter%2==0:
+					if topicFromFriend==1:
+						new_rows_list.append((str(row).strip('[]')).strip("''"))		
+				elif isFriend(row[0]) == True:
+					new_rows_list.append("<b>" + (str(row).strip('[]')).strip("''") + "</b>")
+					topicFromFriend = 1
+				else: #the person is not a friend, so next line not to be appended
+					topicFromFriend =0 			
+				counter=counter+1	
+		counter = 0
+		for feed in reversed(new_rows_list):
+			if counter < 20: #counting until 20 because it should be the 20 last fields
+ 				print_feed.append(feed)
+				counter = counter+1
+		print_feed.reverse() #because right now it is topic - user, rather than user, topic
+		print "<br>".join(map(str, print_feed))
+	else: #if there is not post/file empty
+		print "<br>"
 
 #appends username and new post to csv
 def addPost():
@@ -61,32 +76,34 @@ def addPost():
 			csvfile.write(postForm.getvalue('joke') + '\n')
 			print "<a href=\"../welcome.html\"> Login to see their posts </a>"
 
-
 #append member name to user's row in members.csv
 def addFriend():
 	if (postForm.getvalue("formname") == "addfriend"):
-		new_rows_list = []
-		with open('../data/members.csv', 'r') as csvfile:
-			reader = csv.reader(csvfile)
-			for row in reader:
-				if row[1] == currentusername:
-					foundFriend = False
-					for index, element in enumerate(row, start=3):
-						if element == postForm.getvalue("friend"):
-							foundFriend = True
-							print "This user is already your friend!"
-					if foundFriend == False:
-						new_row = row + postForm.getvalue("friend").split()
-						new_rows_list.append(new_row)
-						print "This user has been added!"
-						print "<a href=\"../welcome.html\"> Login to see their posts </a>"
+		if isMember(postForm.getvalue("friend")) == False:
+			print "This user is not a member."
+		else:
+			new_rows_list = []
+			with open('../data/members.csv', 'r') as csvfile:
+				reader = csv.reader(csvfile)
+				for row in reader:
+					if row[1] == currentusername: #If it is on the current user's row
+						foundFriend = False
+						for index, element in enumerate(row, start+3):
+							if element == postForm.getvalue("friend"):
+								foundFriend = True
+								print "This user is already your friend."
+						if foundFriend == False
+							new_row = row + postForm.getvalue("friend").split()
+							new_rows_list.append(new_row)
+							print "This user has been added."
+							print "<a href=\"../welcome.html\"> Login again to see their posts </a>"
+						else:
+							new_rows_list.append(row)
 					else:
 						new_rows_list.append(row)
-				else:
-					new_rows_list.append(row)
-		with open('../data/members.csv','w') as csvfile:
-				writer = csv.writer(csvfile)
-				writer.writerows(new_rows_list)
+			with open('../data/members.csv','w') as csvfile:
+					writer = csv.writer(csvfile)
+					writer.writerows(new_rows_list)
 
 print"Content-Type: text/html\n\n"
 print """
